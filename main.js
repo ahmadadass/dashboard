7e6d3d7c6c450fc3bfb23810037a94db9c0bc787
root <root@desktop>
2024-11-10 15:36:15 +0200

    fixing ui and ux test75

--- main.js
+++ main.js
@@ -66,10 +66,10 @@ function setViewMain(){
         <div class="loader"></div>
         <div class="row">
             <div class="column">
-                <div id="chart_user" style="width: 450px; height: 250px;"></div>
+                <div id="chart_user" style="width: 500px; height: 250px;"></div>
             </div>
             <div class="column">
-                <div id="chart_cases" style="width: 450px; height: 250px;"></div>
+                <div id="chart_cases" style="width: 500px; height: 250px;"></div>
             </div>
         </div>
         `
@@ -122,11 +122,11 @@ async function drawChart() {
         <div class="row">
             <div class="column">
                 <h1>عدد المستخدمين في العمادات</h1>
-                <div id="chart_user" style="width: 450px; height: 250px;"></div>
+                <div id="chart_user" style="width: 500px; height: 250px;"></div>
             </div>
             <div class="column">
                 <h1>القضايا الفنية</h1>
-                <div id="chart_cases" style="width: 450px; height: 250px;"></div>
+                <div id="chart_cases" style="width: 500px; height: 250px;"></div>
             </div>
         </div>
         `
